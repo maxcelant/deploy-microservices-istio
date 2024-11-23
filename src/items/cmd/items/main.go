c0a09f318b4811bc0c2076b5bf93e75d6a865869
Max Celant <massimiliano.celant@aa.com>
2024-11-22 22:31:38 -0600

    Update to use gorilla mux in the services

--- src/items/cmd/items/main.go
+++ src/items/cmd/items/main.go
@@ -6,26 +6,29 @@ import (
 	"os"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/maxcelant/istio-microservice-sample-items/internal/svc"
 )
 
 func main() {
-	lg := log.New(os.Stdout, "items-svc", log.LstdFlags)
-	users, err := svc.LoadItems()
+	lg := log.New(os.Stdout, "items-svc ", log.LstdFlags)
+	items, err := svc.LoadItems()
 	if err != nil {
 		lg.Fatalf("Error loading JSON: %v", err)
 	}
 
-	sm := http.NewServeMux()
-	sm.Handle("/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
-	}))
-	sm.Handle("/api/items", svc.ItemsHandler(lg, users))
-	sm.Handle("/api/item", svc.ItemHandler(lg, users))
+	}).Methods(http.MethodGet)
+
+	router.HandleFunc("/api/items", svc.ItemsHandler(lg, items)).Methods(http.MethodGet)
+	router.HandleFunc("/api/items/{id}", svc.ItemHandler(lg, items)).Methods(http.MethodGet)
 
 	s := &http.Server{
 		Addr:         ":8081",
-		Handler:      sm,
+		Handler:      router,
 		ErrorLog:     lg,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
--- src/users/cmd/users/main.go
+++ src/users/cmd/users/main.go
@@ -6,26 +6,29 @@ import (
 	"os"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/maxcelant/istio-microservice-sample-users/internal/svc"
 )
 
 func main() {
-	lg := log.New(os.Stdout, "users-svc", log.LstdFlags)
+	lg := log.New(os.Stdout, "users-svc ", log.LstdFlags)
 	users, err := svc.LoadUsers()
 	if err != nil {
 		lg.Fatalf("Error loading JSON: %v", err)
 	}
 
-	sm := http.NewServeMux()
-	sm.Handle("/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
-	}))
-	sm.Handle("/api/users", svc.UsersHandler(lg, users))
-	sm.Handle("/api/user", svc.UserHandler(lg, users))
+	}).Methods(http.MethodGet)
+
+	router.HandleFunc("/api/users", svc.UsersHandler(lg, users)).Methods(http.MethodGet)
+	router.HandleFunc("/api/users/{id}", svc.UserHandler(lg, users)).Methods(http.MethodGet)
 
 	s := &http.Server{
 		Addr:         ":8080",
-		Handler:      sm,
+		Handler:      router,
 		ErrorLog:     lg,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
